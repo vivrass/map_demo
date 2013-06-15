d1d0b421a4b6bcbe3f32821093edfa6e1650271d
Martin Provencher <mprovencher86@gmail.com>
2013-06-15 17:24:32 -0400

    First commit with 'Rails new' and the gems

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem "therubyracer"
+  gem "less-rails"
+  gem "twitter-bootstrap-rails"
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+gem 'gmaps4rails'
+
+group :developement, :test do
+  gem 'debugger'
+
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = '96b0a4820186c10f8ccfb91400ebed08b554d91472d51223fb2dd3b2f722048417d8cbb18808b3a62d2b0b692be9d40dbb749561529e04365a71eaf609d10c8a'
